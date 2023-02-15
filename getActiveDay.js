4add0727c3732a789a5dfd8e5edc6cf72e2ad861
Diego Couto <dev.diegocouto@gmail.com>
2023-02-14 21:20:29 -0300

    Initial commit

---
+++ getActiveDay.js
@@ -0,0 +1,6 @@
+function getActiveDay(date) {
+    const day = new Date(year, month, date);
+    const dayName = day.toString().split(" ")[0];
+    eventDay.innerHTML = dayName;
+    eventDate.innerHTML = date + ' ' + months[month] + ' ' + year;
+}
