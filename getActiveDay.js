c204a2f6e0e6103ed4e19e786a9b1fe46655dac5
Diego Couto <dev.diegocouto@gmail.com>
2025-01-24 00:55:02 -0300

    Update getActiveDay.js
    mudando para data local

--- getActiveDay.js
+++ getActiveDay.js
@@ -1,6 +1,7 @@
 function getActiveDay(date) {
     const day = new Date(year, month, date);
-    const dayName = day.toString().split(" ")[0];
+    // Usando toLocaleDateString() para exibir o nome do dia em português
+    const dayName = day.toLocaleDateString('pt-BR', { weekday: 'short' }); // Exibe o nome do dia em português (abreviado)
     eventDay.innerHTML = dayName;
     eventDate.innerHTML = date + ' ' + months[month] + ' ' + year;
 }
